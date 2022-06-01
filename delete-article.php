39b7a13b9ce75c5fae2e5271ed57d0be9145578a
VirginieBroye <v.broye@free.fr>
2022-06-01 10:56:47 +0200

    CRUD sur la base de donnée

--- delete-article.php
+++ delete-article.php
@@ -1,19 +1,28 @@
 <?php
 
-$filename = __DIR__.'/data/articles.json';
-$articles = [];
+/**
+ * @var PDO
+ */
+$pdo = require_once './database.php';
+$statement = $pdo->prepare('DELETE FROM article WHERE id=:id');
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
 
-if (!$id) {
-    header('Location: /');
-} else {
-    if (file_exists($filename)) {
-        $articles = json_decode(file_get_contents($filename), true) ?? [];
-        $articleIdx = array_search($id, array_column($articles, 'id'));
-        array_splice($articles, $articleIdx, 1);
-        file_put_contents($filename, json_encode($articles));
-        header('Location: /');
-    }
+// if (!$id) {
+//     header('Location: /');
+// } else {
+//     if (file_exists($filename)) {
+//         $articles = json_decode(file_get_contents($filename), true) ?? [];
+//         $articleIdx = array_search($id, array_column($articles, 'id'));
+//         array_splice($articles, $articleIdx, 1);
+//         file_put_contents($filename, json_encode($articles));
+//         header('Location: /');
+//     }
+// }
+
+if($id) {
+    $statement->bindValue(':id', $id);
+    $statement->execute();
 }
+header('Location: /');
--- form-article.php
+++ form-article.php
@@ -1,11 +1,24 @@
 <?php
+
+/**
+ * @var PDO
+ */
+$pdo = require_once './database.php';
+$statementCreateOne = $pdo->prepare('
+    INSERT INTO article (title, category, content, image) VALUES (:title, :category, :content, :image)
+');
+$statementUpdateOne = $pdo->prepare('
+    UPDATE article SET title=:title, category=:category, content=:content, image=:image WHERE id=:id
+');
+$statementReadOne = $pdo->prepare('SELECT * FROM article WHERE id=:id');
+
 const ERROR_REQUIRED = "Veuillez renseigner ce champ";
 const ERROR_TITLE_TOO_SHORT = "Le titre est trop court";
 const ERROR_CONTENT_TOO_SHORT = "L'article est trop court";
 const ERROR_IMAGE_URL = "L'image doit être une url valide";
 
-$filename = __DIR__ . '/data/articles.json';
-$articles = [];
+// $filename = __DIR__ . '/data/articles.json';
+// $articles = [];
 $category = '';
 
 $errors = [
@@ -15,16 +28,19 @@ $errors = [
     'content' => ''
 ];
 
-if (file_exists($filename)) {
-    $articles = json_decode(file_get_contents($filename), true) ?? [];
-}
+// if (file_exists($filename)) {
+//     $articles = json_decode(file_get_contents($filename), true) ?? [];
+// }
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
 
 if ($id) {
-    $articleIdx = array_search($id, array_column($articles, 'id'));
-    $article = $articles[$articleIdx];
+    $statementReadOne->bindValue(':id', $id);
+    $statementReadOne->execute();
+    $article = $statementReadOne->fetch();
+    // $articleIdx = array_search($id, array_column($articles, 'id'));
+    // $article = $articles[$articleIdx];
 
     $title = $article['title'];
     $image = $article['image'];
@@ -74,21 +90,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
 
         if ($id) {
-            $articles[$articleIdx]['title'] = $title;
-            $articles[$articleIdx]['image'] = $image;
-            $articles[$articleIdx]['category'] = $category;
-            $articles[$articleIdx]['content'] = $content;
-        } else {
-            $articles = [...$articles, [
-                'title' => $title,
-                'image' => $image,
-                'category' => $category,
-                'content' => $content,
-                'id' => time()
-            ]];
+            // $article['title'] = $title;
+            // $article['image'] = $image;
+            // $article['category'] = $category;
+            // $article['content'] = $content;
+//On met à jour la bdd
+            $statementUpdateOne->bindValue(':title', $title);
+            $statementUpdateOne->bindValue(':category', $category);
+            $statementUpdateOne->bindValue(':content', $content); 
+            $statementUpdateOne->bindValue(':image', $image); 
+            $statementUpdateOne->bindValue(':id', $id);
+            $statementUpdateOne->execute();
+        } else { //creer un new article
+            // $articles = [...$articles, [
+            //     'title' => $title,
+            //     'image' => $image,
+            //     'category' => $category,
+            //     'content' => $content,
+            //     'id' => time()
+            // ]];
+            $statementCreateOne->bindValue(':title', $title);
+            $statementCreateOne->bindValue(':category', $category);
+            $statementCreateOne->bindValue(':content', $content); 
+            $statementCreateOne->bindValue(':image', $image);
+            $statementCreateOne->execute();
         }
 
-        file_put_contents($filename, json_encode($articles));
+        // file_put_contents($filename, json_encode($articles));
         header('Location: /');
     }
 }
